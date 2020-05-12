f9b396072676e8ef1e8a88883a4e91bbc3c24044
adam <adam.parker@nowhealthcaregroup.com>
2020-05-12 10:51:19 +0100

    Initial Commit

---
+++ SearchTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchTest
+//
+//  Created by Adam Parker on 12/05/2020.
+//  Copyright © 2020 Adam Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
